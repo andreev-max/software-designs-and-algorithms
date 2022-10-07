ed2a7800aa1db512a15a3981ba6fea9b8315beb9
Max Andreev <Maksim_Andreyeu@epam.com>
2022-10-07 20:24:37 +0300

    feat: add implementation of the first task

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
@@ -1,1 +1,31 @@
-export class Point {}
+export class Point {
+  readonly x: number;
+  readonly y: number;
+
+  constructor();
+  constructor(x?: number, y?: number);
+  constructor(x?: number, y?: number) {
+    this.x = x ?? 0;
+    this.y = y ?? 0;
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y})`;
+  }
+
+  public distance(): number;
+  public distance(other?: Point): number;
+  public distance(x?: Point, y?: number): number {
+    let otherX = 0;
+    let otherY = 0;
+    if (typeof x === "number" && typeof y === "number") {
+      otherX = x;
+      otherY = y;
+    } else if (typeof x?.x === "number" && typeof x?.y === "number") {
+      otherX = x?.x;
+      otherY = x?.y;
+    }
+
+    return Math.round(Math.sqrt((this.x - otherX) ** 2 + (this.y - otherY) ** 2) * 1000) / 1000;
+  }
+}
--- docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,3 +1,56 @@
+import { Point } from "./Point";
+
+const defaultColor = "green";
+const defaultFilled = true;
+
 export abstract class Shape {
+  protected color: string;
+  protected filled: boolean;
+  protected points: Point[];
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color?: string, filled?: boolean);
+  constructor(points: Point[], color?: string, filled?: boolean) {
+    if (points.length < 3) {
+      throw new Error("You should set at least 3 points");
+    } else {
+      this.points = points;
+    }
+
+    this.color = typeof color === "string" ? color : defaultColor;
+    this.filled = typeof filled === "boolean" ? filled : defaultFilled;
+  }
+
+  public toString() {
+    const isShapeFilled = this.filled ? "filled" : "not filled";
+
+    const pointsInfo = this.points.map(({ x, y }, index) => {
+      if (index === this.points.length - 1) {
+        return ` (${x}, ${y}).`;
+      }
+
+      return ` (${x}, ${y})`;
+    });
+
+    return `A Shape with color of ${this.color} and ${isShapeFilled}. Points:${pointsInfo}`;
+  }
+
+  public getPerimeter(): number {
+    let test = 0;
+
+    for (let i = 0; i < this.points.length; i++) {
+      const firstPoint = this.points[0];
+      const nextPoint = this.points[i + 1];
+      const currentPoint = this.points[i];
+      if (nextPoint) {
+        test = test + currentPoint.distance(nextPoint);
+      } else {
+        test = test + currentPoint.distance(firstPoint);
+      }
+    }
+
+    return test;
+  }
+
   abstract getType(): string;
 }
