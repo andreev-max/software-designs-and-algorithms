b3a7624cd99432b38eb78d817a28de263ad8d12a
Max Andreev <Maksim_Andreyeu@epam.com>
2022-12-01 09:44:46 +0300

    feat: add optimized priority queue

---
+++ docs/8-algorithms-and-data-structures-part-2/priority-queue-task/src/OptimizedPriorityQueue.ts
@@ -0,0 +1,79 @@
+import { PrioritizedItem } from "./types";
+
+export class OptimizedPriorityQueue {
+  public list: PrioritizedItem[];
+  constructor() {
+    this.list = [{ value: "empty", priority: -1000000 }];
+  }
+
+  enqueue(value: string, priority = 0) {
+    this.list.push({ value, priority });
+
+    if (this.list.length > 2) {
+      let i = this.list.length - 1;
+      while (this.list[i].priority < this.list[Math.floor(i / 2)].priority) {
+        // console.log(this.list);
+        [this.list[Math.floor(i / 2)], this.list[i]] = [
+          this.list[i],
+          this.list[Math.floor(i / 2)],
+        ];
+
+        if (Math.floor(i / 2) > 1) {
+          i = Math.floor(i / 2);
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  dequeue() {
+    let smallest = this.list[1];
+    if (this.list.length > 2) {
+      this.list[1] = this.list[this.list.length - 1];
+      this.list.splice(this.list.length - 1);
+
+      if (this.list.length === 3) {
+        if (this.list[1].priority > this.list[2].priority) {
+          [this.list[1], this.list[2]] = [this.list[2], this.list[1]];
+        }
+
+        return smallest;
+      }
+
+      let i = 1;
+      let left = 2 * i;
+      let right = 2 * i + 1;
+
+      while (
+        this.list[i].priority >= this.list[left].priority ||
+        this.list[i].priority >= this.list[right].priority
+      ) {
+        if (this.list[left].priority < this.list[right].priority) {
+          [this.list[i], this.list[left]] = [this.list[left], this.list[i]];
+          i = 2 * i + 1;
+        } else {
+          [this.list[i], (this.list[right] = this.list[right]), this.list[i]];
+          i = 2 * i + 1;
+        }
+
+        left = 2 * i;
+        right = 2 * i + 1;
+
+        if (!this.list[left] || !this.list[right]) {
+          break;
+        }
+      }
+    } else if (this.list.length === 2) {
+      this.list.splice(1, 1);
+    } else {
+      return null;
+    }
+
+    return smallest;
+  }
+
+  print() {
+    console.log(this.list);
+  }
+}
--- docs/8-algorithms-and-data-structures-part-2/priority-queue-task/src/index.ts
+++ docs/8-algorithms-and-data-structures-part-2/priority-queue-task/src/index.ts
@@ -1,18 +1,49 @@
 import { Queue } from "./Queue";
 import { PriorityQueue } from "./PriorityQueue";
+import { OptimizedPriorityQueue } from "./OptimizedPriorityQueue";
 
 const pq = new PriorityQueue();
+const opq = new OptimizedPriorityQueue();
 
-pq.enqueue("10");
-pq.enqueue("50", 10);
-pq.enqueue("30", 11);
-pq.enqueue("40", 144);
-pq.enqueue("50", 3);
-pq.print();
-pq.enqueue("0", 20);
-pq.enqueue("10", 45);
-pq.dequeue();
-pq.enqueue("10", 3);
-pq.enqueue("10", 34);
-pq.dequeue();
-pq.print();
+// pq.enqueue("10");
+// pq.enqueue("50", 10);
+// pq.enqueue("30", 11);
+// pq.enqueue("40", 144);
+// pq.enqueue("50", 3);
+// pq.print();
+// pq.enqueue("0", 20);
+// pq.enqueue("10", 45);
+// pq.dequeue();
+// pq.enqueue("10", 3);
+// pq.enqueue("10", 34);
+// pq.dequeue();
+// pq.print();
+
+// opq.enqueue("10", 10);
+// opq.enqueue("10", 1);
+// opq.enqueue("10", 445);
+// opq.enqueue("10", 35);
+// opq.enqueue("10", 90);
+// opq.enqueue("10", 2);
+// opq.enqueue("10", 19);
+// opq.enqueue("10", 100);
+// opq.enqueue("10");
+// opq.dequeue();
+// opq.enqueue('10')
+
+let pqStart = Date.now();
+
+for (let i = 0; i < 100001; i++) {
+  pq.enqueue("10", Math.floor(Math.random() * 10000));
+}
+const pqEnd = Date.now() - pqStart;
+// opq.print();
+
+let opqStart = Date.now();
+
+for (let i = 0; i < 100001; i++) {
+  opq.enqueue("10", Math.floor(Math.random() * 10000));
+}
+const opqEnd = Date.now() - opqStart;
+// opq.print();
+console.log({ opqEnd, pqEnd });
